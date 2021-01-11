6fb6c709b98226bf05410fc48b5ffa23c2d86883
Tipu Danny <tipu.dany@gmail.com>
2021-01-11 12:15:26 +0600

    Update mySql.php

--- mySql.php
+++ mySql.php
@@ -1,14 +1,15 @@
 <?php 
 <p>Select employee where there name have a vowel.</p>
 
-=> SELECT name FROM employee_list WHERE name REGEXP "[aeiou].*[aeiou]"
+$ans = SELECT name FROM employee_list WHERE name REGEXP "[aeiou].*[aeiou]";
 
 <p>Select employee where there name start with  a vowel.</p>
 
-=> SELECT name FROM employee_list WHERE name REGEXP "^[aeiou].*[aeiou]"
+$ans =  SELECT name FROM employee_list WHERE name REGEXP "^[aeiou].*[aeiou]";
 
 <p>Select employee where there name End with  a vowel.</p>
-=> SELECT name FROM employee_list WHERE name REGEXP "[aeiou].*[aeiou]$"
+SELECT name FROM employee_list WHERE name REGEXP "[aeiou].*[aeiou]$";
 
 <p>Select employee where there name Start and End with  a vowel.</p>
-=> SELECT name FROM employee_list WHERE name REGEXP "^[aeiou].*[aeiou]$"
+$ans =  SELECT name FROM employee_list WHERE name REGEXP "^[aeiou].*[aeiou]$";
+?>
